91990a26d16117b339e512d2ece9584c8c466430
20IT120-JIMMY <20it120@charusat.edu.in>
2022-09-15 23:25:03 +0530

    Commit with navigation , footer & MyAlerts

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Student from "./components/Student";
+import StudentList from "./components/StudentList";
+import NavigationBar from "./components/NavigationBar";
+import Footer from "./components/Footer";
+import Container from "react-bootstrap/Container";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MyAlert from "./components/MyAlert";
+import { useState } from "react";
+import Login from "./components/Login";
 
 function App() {
+  const [alert, setAlert] = useState(null);
+
+  let showAlert = (type, message) => {
+    setAlert({
+      type: type,
+      message: message,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 3000);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <NavigationBar />
+        <MyAlert alert={alert} />
+        <Footer />
+      </Router>
     </div>
   );
 }
